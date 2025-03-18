e48c16328b69d405a7222a5af6b98ce38bcab423
loganS475 <los0017@auburn.edu>
2025-03-18 16:53:12 -0500

    Neural Network Class1

--- Dataframe.py
+++ Dataframe.py
@@ -13,3 +13,32 @@ training = df[:4089]
 testing = df[4089:]
 print(training)
 print(testing)
+
+#Checks if a Cuda device is avaliable otherwise defaults to CPU
+if torch.accelerator.is_available():
+    device = torch.accelerator.current_accelerator().type
+else:
+    device = "cpu"
+print(f"Using {device} device")
+
+# Class for Neural Network
+class NeuralNetwork(nn.Module):
+    #sets up constructor for Neural Network
+    def __init__(self):
+        super().__init__()# check to make sure constructor is properly set up
+        self.flatten = nn.Flatten() # Reshapes data into a vector
+        self.linear_relu_stack = nn.Sequential( #Builds hidden and visible layers of neural network
+            nn.Linear(12, 32),
+            nn.ReLU(),
+            nn.Linear(32, 16),
+            nn.ReLU(),
+            nn.Linear(32, 8),
+        )
+
+    #shows how data flows through neural network
+    def forward(self, x):
+        x = self.flatten(x) #flattens data so it can enter neural network
+        logits = self.linear_relu_stack(x)# runs data through layers and returns data unnormalized
+        return logits
+    
+    
